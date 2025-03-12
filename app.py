d95d4cb8e0d2f53336f6255163fd5d31a3351222
goat du site <72492241+aguacero7@users.noreply.github.com>
2025-03-12 12:00:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from app import create_app # import de la fonction create_app du fichier app/__init__.py
 from app.extensions import db, migrate # import des extensi
-from DEPSECModels import *
+#from DEPSECModels import *
 
 app = create_app()
 
